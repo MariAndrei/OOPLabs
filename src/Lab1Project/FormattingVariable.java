f8b3ed642f76b8ea48a1e3f6e4de121fa9bb16e8
MariAndrei <marian.a.rogojina@students.ittralee.ie>
2024-09-13 10:38:30 +0100

    12

--- src/Lab1Project/FormattingVariable.java
+++ src/Lab1Project/FormattingVariable.java
@@ -8,7 +8,7 @@ public class FormattingVariable {
         int population1 = 7092326;
         double populationDensity1 = 82.2;
 
-        String country2 = "United kingdom";
+        String country2 = "United Kingdoom";
         int population2 = 6092326;
         double populationDensity2 = 270.7;
 
